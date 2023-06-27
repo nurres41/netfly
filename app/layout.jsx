b512281834771bdd6c61ad350fdbbafbfbbcf1ed
Nuri Onur <nurionurkurtulus@gmail.com>
2023-06-27 13:54:25 +0300

    Changed font family

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-import { Inter } from "@next/font/google";
+
 
 import "@/styles/reset.css";
 import "@/styles/global.css";
@@ -6,11 +6,11 @@ import "@/styles/global.css";
 import  Header  from "@/components/header";
 import  Footer  from "@/components/footer";
 
-const interFontFamily = Inter({ subsets: ['latin'] });
+
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={interFontFamily.className}>
+    <html lang="en" >
       <body className="container">
         <Header />
         <main>{children}</main>
